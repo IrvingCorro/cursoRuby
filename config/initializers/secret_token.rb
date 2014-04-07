7d71a1e0a6424dcfcee98f192979fa5cc2f0c317
irving <irvingsk19@hotmail.com>
2014-04-06 21:31:36 -0500

    primer comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CursoRuby::Application.config.secret_key_base = 'd9b47de322cf50475c16c2fd18b8ea4626a44ed588f6d8482a991d528c7c1896ed371d9ac26c0ba9852256f8d404c6f1c5462b096e1cc396b15886f34215483b'
